eb6a7d2bf9388e3f48e6c84dd44710e00a67aa9d
Hiboabdi21 <ahibo1793@gmail.com>
2022-07-15 08:32:23 -0700

    Added testcase for checking email and password

--- test_project.py
+++ test_project.py
@@ -1,1 +1,18 @@
 import pytest
+import project
+
+# Testcase for checking Invalid Email
+def test_getEmail1():
+   assert project.getEmail("@testWrongEmail") == "Invalid Email"
+
+# Testcase for checking valid Email
+def test_getEmail2():
+   assert project.getEmail("testCorrectmail@gmail.com") == "testCorrectmail@gmail.com"
+
+# Testcase for checking Invalid Password
+def test_getPassword1():
+   assert project.getPassword("123") == "Invalid Password"
+
+# Testcase for checking valid Email
+def test_getPassword2():
+   assert project.getPassword("test1234") == "test1234"
